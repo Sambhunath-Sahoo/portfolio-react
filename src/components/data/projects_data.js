3e0f3b2c3316a2471b10006f637ca1aa97aac654
Sambhu <sambhunathsahoo25@gmail.com>
2021-08-15 10:39:12 +0530

    added new projects

--- src/components/data/projects_data.js
+++ src/components/data/projects_data.js
@@ -1,10 +1,36 @@
 import cricinfo from '../../assets/projects_images/Cricinfo.PNG'
 import onesheet from '../../assets/projects_images/onesheet.PNG'
 import todo from '../../assets/projects_images/todo.PNG'
+import slack from "../../assets/projects_images/slack.PNG"
+import netflix from "../../assets/projects_images/netflix.PNG"
+import Spotify_UI from "../../assets/projects_images/spotify.PNG"
+import hackerrank from "../../assets/projects_images/hackerrank.PNG"
+import interviewbit from "../../assets/projects_images/interviewbit.PNG"
 
 
 // ! add projects here
 const data_projects = [
+    {
+        name: 'Slack-mini',
+        image: slack,
+        deployed_url: 'https://slack-clone-2cee4.web.app/',
+        github_url: 'https://github.com/Sambhunath-Sahoo/slack-clone',
+        category: ['Reactjs', 'node.js', 'ContextAPI']
+    },
+    {
+        name: 'Spotify_UI',
+        image: Spotify_UI,
+        deployed_url: 'https://github.com/Sambhunath-Sahoo/spotify-clone',
+        github_url: 'https://github.com/Sambhunath-Sahoo/spotify-clone',
+        category: ['Reactjs', 'node.js', 'ContextAPI']
+    },
+    {
+        name: 'Netflix_clone',
+        image: netflix,
+        deployed_url: 'https://netflix-clone-9510f.web.app/',
+        github_url: 'https://github.com/Sambhunath-Sahoo/netflix-clone',
+        category:['Reactjs', 'node.js', 'ContextAPI']
+    },
     {
         name: 'One-Sheet Online',
         image: onesheet,
@@ -18,6 +44,22 @@ const data_projects = [
         image: cricinfo,
         deployed_url: '',
         github_url: 'https://github.com/Sambhunath-Sahoo/Web_Scrapper-Cricinfo',
+        category: ['node.js', '']
+    },
+
+    {
+        name: 'hackerrank submitter',
+        image: hackerrank,
+        deployed_url: '',
+        github_url: 'https://github.com/Sambhunath-Sahoo/hackerrank-code-submitter',
+        category: ['node.js', '']
+    },
+
+    {
+        name: 'interviewbit Scrapper',
+        image: interviewbit,
+        deployed_url: '',
+        github_url: 'https://github.com/Sambhunath-Sahoo/interviewBit-scraper',
         category: ['node.js', '']
     },
 
