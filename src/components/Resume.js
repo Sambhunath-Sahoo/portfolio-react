1e7b485380b8dadf72ba8db380b99bc7a3ff13c2
Sambhu <sambhunathsahoo25@gmail.com>
2021-09-10 21:32:42 +0530

    changed

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,90 +1,78 @@
-import React from 'react';
+import React from "react";
 
-import Bar from './Bar';
-import { motion } from 'framer-motion'
-import { tools, languages } from './data/resume_data'
-
+import Bar from "./Bar";
+import { motion } from "framer-motion";
+import { tools, languages } from "./data/resume_data";
 
 const Resume = () => {
-
     const resume_variants = {
         hidden: {
-            opacity: 0
+            opacity: 0,
         },
         visible: {
             opacity: 1,
             transition: {
-                delay: 0.2, duration: 0.6,
-            }
+                delay: 0.2,
+                duration: 0.6,
+            },
         },
         exit: {
             opacity: 0,
             transition: {
-                ease: 'easeInOut'
-            }
-        }
-    }
+                ease: "easeInOut",
+            },
+        },
+    };
     return (
-        <motion.div className="container resume"
-
+        <motion.div
+            className="container resume"
             variants={resume_variants}
             initial="hidden"
             animate="visible"
             exit="exit"
-
         >
             <div className="row">
-                <div className="col-lg-6 resume-card" >
-                    <h4 className="resume-card__heading">
-                        Education
-                    </h4>
+                <div className="col-lg-6 resume-card">
+                    <h4 className="resume-card__heading">Education</h4>
                     <div className="resume-card__body">
-                        <h5 className="resume-card__title">
-                            Computer Science Enginnering
-                        </h5>
+                        <h5 className="resume-card__title">Computer Science Enginnering</h5>
                         <p className="resume-card__name">
                             Gandhi institue of Engineering and Technology, Gunupur(2017-2021)
                         </p>
-                        <p className="resume-card__details">I am currently persuing 4th of B.tech in Computer Science Engineering from GIET University</p>
+                        <p className="resume-card__details">
+                            I am currently persuing 4th of B.tech in Computer Science
+                            Engineering from GIET University
+                        </p>
                     </div>
                 </div>
-                <div className="col-lg-6 resume-card" >
-                    <h4 className="resume-card__heading">
-                        Experience
-                    </h4>
+                <div className="col-lg-6 resume-card">
+                    <h4 className="resume-card__heading">Experience</h4>
                     <div className="resume-card__body">
-                        <h5 className="resume-card__title">
-                            Intern
-                        </h5>
-                        <p className="resume-card__name">
-                            SISCASO
+                        <h5 className="resume-card__title">Intern</h5>
+                        <p className="resume-card__name">SISCASO</p>
+                        <p className="resume-card__details">
+                            I work as a intern in SISCASO and also completed some projects on
+                            Web Development
                         </p>
-                        <p className="resume-card__details">I work as a intern in SISCASO and also completed some projects on Web Development</p>
                     </div>
                 </div>
             </div>
 
             <div className="row">
                 <div className="col-lg-6 resume-languages">
-                    <h5 className="reume-language__heading">
-                        Language and Framework
-                    </h5>
+                    <h5 className="reume-language__heading">Language and Framework</h5>
                     <div className="mt-3 resume-language__body">
-                        {
-                            languages.map(language =>
-                                <Bar value={language} />
-                            )
-                        }
+                        {languages.map((language) => (
+                            <Bar value={language} />
+                        ))}
                     </div>
                 </div>
                 <div className="col-lg-6 resume-languages">
-                    <h5 className="reume-language__heading">
-                        Tools and Softwares
-                    </h5>
+                    <h5 className="reume-language__heading">Tools and Softwares</h5>
                     <div className="mt-3 resume-language__body">
-                        {
-                            tools.map(tool => <Bar value={tool} />)
-                        }
+                        {tools.map((tool) => (
+                            <Bar value={tool} />
+                        ))}
                     </div>
                 </div>
             </div>
