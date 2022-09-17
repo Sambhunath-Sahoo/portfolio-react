08821f64beac45647c11a92fcd9da313f78f7d50
Sambhu <sambhu05357@gmail.com>
2022-09-17 12:05:56 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -79,8 +79,7 @@ const Sidebar = () => {
                 <div className="sidebar__item sidebar__location">
                     <b>Odisha, India</b>
                 </div>
-                <div className="sidebar__item"><b>+91-9078164602</b></div>
-                {/* <div className="sidebar__item">sambhunathsahoo25@gmail.com</div> */}
+                <div className="sidebar__item">sambhu05357@gmail.com</div>
             </div>
             {/* <div className="sidebar__item sidebar__theme" onClick={handleTheme}>change theme</div> */}
             {/* <div className="sidebar__item sidebar__email" onClick={handleEmailMe}>email me</div> */}
